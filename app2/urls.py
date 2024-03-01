fed802b1d94e272658b209876988598afe224207
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-01 17:27:36 +0530

    Authentication and Permissions  committed by diya patel

---
+++ app2/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from app2 import views
+from rest_framework.routers import DefaultRouter
+
+# Creating Router Object
+router = DefaultRouter()
+
+# Register StudentViewSet with Router
+router.register('studentapi', views.StudentModelViewSet, basename='student')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
