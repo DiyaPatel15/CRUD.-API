911a26038553ee712672bb7b3c7119b4420d2d65
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-01 18:07:31 +0530

    DjangoModelPermissions  committed by diya patel

--- app2/urls.py
+++ app2/urls.py
@@ -10,4 +10,5 @@ router.register('studentapi', views.StudentModelViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/',include('rest_framework.urls', namespace='rest_framework'))
 ]
