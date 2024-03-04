50a51a616d7c536c6b149595c7a0ef8d455dd48c
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-04 16:59:15 +0530

    TokenAuthentication committed by diya patel

--- app2/urls.py
+++ app2/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from app2 import views
 from rest_framework.routers import DefaultRouter
-
+# from rest_framework.authtoken.views import obtain_auth_token
+from app2.auth import CustomAuthToken
 # Creating Router Object
 router = DefaultRouter()
 
@@ -10,5 +11,7 @@ router.register('studentapi', views.StudentModelViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/',include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/',include('rest_framework.urls', namespace='rest_framework')),
+    # path('gettoken/',obtain_auth_token)
+    path('gettoken/',CustomAuthToken.as_view())
 ]
--- app2/views.py
+++ app2/views.py
@@ -1,19 +1,22 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication,SessionAuthentication
+from rest_framework.authentication import BasicAuthentication,SessionAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 from .custompermissions import MyPermission
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     # authentication_classes = [BasicAuthentication]
-    authentication_classes = [SessionAuthentication]
+    # authentication_classes = [SessionAuthentication]
     # permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
     # permission_classes = [IsAuthenticatedOrReadOnly]
     # permission_classes = [DjangoModelPermissions]
     # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    permission_classes = [MyPermission]
+    # permission_classes = [MyPermission]
+
+
+
 
