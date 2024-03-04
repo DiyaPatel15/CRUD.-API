34155782dd20c3b9a786a22f1afbc6c52068928d
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-04 18:16:30 +0530

    TokenAuthentication committed by diya patel

--- app2/urls.py
+++ app2/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from app2 import views
 from rest_framework.routers import DefaultRouter
 # from rest_framework.authtoken.views import obtain_auth_token
-from app2.auth import CustomAuthToken
+# from app2.auth import CustomAuthToken
 # Creating Router Object
 router = DefaultRouter()
 
@@ -13,5 +13,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/',include('rest_framework.urls', namespace='rest_framework')),
     # path('gettoken/',obtain_auth_token)
-    path('gettoken/',CustomAuthToken.as_view())
+    # path('gettoken/',CustomAuthToken.as_view())
 ]
