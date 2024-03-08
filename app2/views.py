5b84a6618311284852f5c98a1ccf2bc7962d6d3c
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-08 15:44:49 +0530

    OFiltering committed by diya patel

--- app2/views.py
+++ app2/views.py
@@ -3,7 +3,7 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView,RetrieveAPIView,DestroyAPIView,CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 
 
@@ -13,11 +13,15 @@ class StudentList(ListAPIView):
     # filter_backends = [DjangoFilterBackend]       #Per View filtering(Locally)
     # filterset_fields = ['city']                   #for  find exact city
     # filterset_fields = ['name','city']            #for  find exact city and name
-    filter_backends = [SearchFilter]
+    # filter_backends = [SearchFilter]
     # search_fields = ['name','city']               #searching for  exact city and name
-    search_fields = ['^name']                     #search = r(first digit of name), start with search
+    # search_fields = ['^name']                     #search = r(first digit of name), start with search
     # search_fields = ['$name']                     # for regex search
     # search_fields = ['@name']                     #for full-text path
+    filter_backends = [OrderingFilter]
+    # ordering_fields = ['city']
+    ordering_fields = ['name''city']
+
 
 
 
