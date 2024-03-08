7da24c5e147ddd8224e514e481c7841672bac9ea
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-08 14:49:29 +0530

    Filtering committed by diya patel

--- app2/views.py
+++ app2/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView,RetrieveAPIView,DestroyAPIView,CreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get_queryset(self):
-        user = self.request.user
-        return Student.objects.filter(passby=user)
+    filter_backends = [DjangoFilterBackend]       #Per View filtering(Locally)
+    filterset_fields = ['name','city']
+
 
 
 
