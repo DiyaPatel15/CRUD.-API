0d7cd79f945c560b231876f3a3f79a46f832e54f
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-11 10:45:06 +0530

    HyperlinkedModelSerializer committed by diya patel

---
+++ app2/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+
+
+
